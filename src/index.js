1a494d97ed09d2cc1cdd831391883ffa628bf9dd
denimcouch <alexmata92@ymail.com>
2020-10-07 10:11:15 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+console.log("Hello")
+
+function fetchData() {
+  let data = fetch('https://raw.githubusercontent.com/denimcouch/ajax-discussion-question-hou01-seng-ft-082420/master/src/index.html')
+    .then( res => res.json() )
+    .then( json => console.log(json) )
+}
+fetchData()
+// console.log("Sup?")
